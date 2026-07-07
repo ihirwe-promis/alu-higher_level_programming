d0d7c12ff7731f3cecdd5b038304a797dbcd3077
ihirwe-promis <p.ihirwe@alustudent.com>
2026-07-07 17:42:01 +0200

    Add replace_in_list function

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module that replaces an element in a list."""
+
+
+def replace_in_list(my_list, idx, element):
+    """Replace an element at a specific index and return the list."""
+    if idx < 0:
+        return my_list
+
+    if idx >= len(my_list):
+        return my_list
+
+    my_list[idx] = element
+    return my_list
