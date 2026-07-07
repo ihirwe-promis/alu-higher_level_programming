73daf21c9994a069973767a3da018894c3eca8d8
ihirwe-promis <p.ihirwe@alustudent.com>
2026-07-07 17:33:25 +0200

    update all files

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module that retrieves an element from a list."""
+
+
+def element_at(my_list, idx):
+    """Return the element at the given index, or None if invalid."""
+    if idx < 0:
+        return None
+
+    if idx >= len(my_list):
+        return None
+
+    return my_list[idx]
