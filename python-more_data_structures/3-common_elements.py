7f675d056b2cf30bc0c26a33a61ab27c6ef5c3d8
ihirwe-promis <p.ihirwe@alustudent.com>
2026-07-08 15:38:13 +0200

    Complete task 3

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module that returns common elements in two sets."""
+
+
+def common_elements(set_1, set_2):
+    """Return a set of elements present in both sets."""
+    return set_1 & set_2
