e1f5f951a5f9de0f72c533a409bd9f2b769b1734
ihirwe-promis <p.ihirwe@alustudent.com>
2026-07-08 14:59:39 +0200

    Add 11-delete-at.py

---
+++ python-data_structures/11-delete-at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module that deletes an item at a specific position in a list."""
+
+
+def delete_at(my_list=[], idx=0):
+    """Delete the item at the given index and return the list."""
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+
+    del my_list[idx]
+
+    return my_list
