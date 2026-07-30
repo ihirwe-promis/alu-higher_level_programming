979c365b448fa7cace53bf8a2101c2be2782f5ce
ihirwe-promis <p.ihirwe@alustudent.com>
2026-07-30 14:46:30 +0200

    solved task 11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Defines a Student class."""
+
+
+class Student:
+    """Represents a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return a dictionary representation of a Student.
+
+        If attrs is a list of strings, only attributes in the list
+        are returned. Otherwise, all attributes are returned.
+        """
+        if isinstance(attrs, list):
+            return {key: self.__dict__[key]
+                    for key in attrs if key in self.__dict__}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace Student attributes from a dictionary."""
+        for key, value in json.items():
+            setattr(self, key, value)
